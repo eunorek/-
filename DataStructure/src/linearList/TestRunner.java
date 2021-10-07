096d319efbd2407432134eba21a7564661903dbf
YeEun Kim <yeeun.kim.yu@gmail.com>
2021-10-07 17:46:54 +0900

    created merge method

---
+++ DataStructure/src/linearList/TestRunner.java
@@ -0,0 +1,13 @@
+package linearList;
+
+import test.LinearListBasicTest;
+
+public class TestRunner {
+
+    public static void main(String[] args) {
+        System.out.println("starting LinearList basic test");
+        LinearListBasicTest test1 = new LinearListBasicTest();
+       
+    }
+
+}
